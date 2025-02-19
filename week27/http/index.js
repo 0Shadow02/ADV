4c586e039580d7a4bd9102b2e54be5f3f2eb4e6a
Shadow <dark35854@gmail.com>
2025-02-19 16:30:36 +0530

    fs

---
+++ week27/http/index.js
@@ -0,0 +1,31 @@
+//  here we are going to learn about the file system module in nodejs
+const fs = require("fs")
+
+// --> write sync ..
+//  fs.writeFileSync("./names.txt" , "shadow\ndark\n")
+
+//  --> write async
+// fs.writeFile("./contact.txt" , "email:\"shadow@gmail.com\" " , (err)=> console.log("error" , err))
+
+// --> read sync ..
+// const data = fs.readFileSync("./secrects.txt" , "utf8")
+// console.log(data)
+
+// --> read async ..
+// fs.readFile("./secrects.txt" , "utf-8" , (err,data)=>{
+//     if (err) {
+//         console.log("err",err)
+//     }
+//     else {
+//          console.log(data)
+//     }
+//   } )
+
+// --> append sync ..
+//   fs.appendFileSync("./names.txt", "ryuji\n")
+
+// --> aapend async ..
+//   fs.appendFile("./contact.txt" , "email:\"dark@gmail.com\"\n" ,(err)=> console.log(err))
+
+
+  console.log("done")
