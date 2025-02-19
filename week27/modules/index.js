161d00715a6397c6485dcdf335d1796984ff88b5
Shadow <dark35854@gmail.com>
2025-02-19 15:21:53 +0530

    modules

---
+++ week27/modules/index.js
@@ -0,0 +1,4 @@
+const math = require("./maths")
+const {name} = require("./maths")
+console.log("hey there login - modlues lec " + name)
+console.log(math.addition(2,5), math.sub(3,1), math.mul(3,3), math.div(6,2))
