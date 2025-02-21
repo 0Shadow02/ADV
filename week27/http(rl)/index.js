60f87b876409729a790ff01e8b9182f06567ed80
Shadow <dark35854@gmail.com>
2025-02-21 14:55:57 +0530

    http

--- week27/http(rl)/index.js
+++ week27/http(rl)/index.js
@@ -2,9 +2,24 @@ const http = require('http')
 const fs = require('fs')
 
 const myserver= http.createServer((req,res)=>{
-    const log = `${Date.now()}: New req recieved \n`
+    const log = `${Date.now()}: New req at ${req.url} recieved \n`
     fs.appendFile("log.txt" , log , (err)=>{
-        res.end("done!")
+        switch (req.url) {
+            case "/":
+                res.end("home page")
+                break;
+            
+            case "/dashboard":
+                res.end("----welcome shadow---- ***")
+                break;
+            case "/about":
+                res.end("hey I am shadow , nice to meet you")
+                break;
+
+            default:
+                res.end("404 not found")
+                break;
+        }
     })
 
 })
