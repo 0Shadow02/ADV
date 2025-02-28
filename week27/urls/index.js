7333168770c0d8d314811a303f0f363afd45690d
Shadow <dark35854@gmail.com>
2025-02-28 17:43:22 +0530

    url

---
+++ week27/urls/index.js
@@ -0,0 +1,28 @@
+const http = require("http")
+const fs = require("fs")
+const url = require("url")
+
+const myServer = http.createServer((req, res)=>{
+    const path = req.url
+    const log = `this person req this url ${path} at this time ${Date.now()}\n`
+    fs.appendFile("log.txt" ,log ,(err)=>{
+        if (err) {
+            console.log(err)
+        }
+    })
+    const myPathlog = url.parse(path, true)
+    console.log(myPathlog)
+    switch (myPathlog.pathname) {
+        case "/":
+            res.end(`${JSON.stringify(myPathlog)}`)
+            break;
+        case "/about":
+            res.end(`hey there my name is shadow`)
+                break; 
+        default:
+            res.end(`${myPathlog}`)
+            break;
+    }
+})
+
+myServer.listen(8000, ()=> console.log("listening to the port 8000"))
